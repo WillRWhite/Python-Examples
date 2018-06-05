74fc80f88c2560c385471450e02353c438202552
William White <will@wwhite.co.uk>
2018-06-05 01:15:06 +0100

    Added string input

--- sources/ran.py
+++ sources/ran.py
@@ -1,3 +1,10 @@
 import random
+response = input("Enter your name: ")
+print("Hello " + response + ", here are 5 random numbers")
+
 for i in range(5):
     print(random.random())
+
+print()
+print("Thanks for running me!!")
+print()
