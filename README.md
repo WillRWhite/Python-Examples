532ec7c74b9d7a329c44e2761cd54af7788c9ab4
William White <will@wwhite.co.uk>
2018-06-05 11:04:34 +0100

    Added string input

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python-Examples
-Python Examples
+
+hello.py    Simple project for playing
+ran.py      Random number generator
--- sources/ran.py
+++ sources/ran.py
@@ -1,3 +1,17 @@
 import random
+yourName = input("Enter your name: ")
+yourAge = input("How old are you: ")
+
+if int(yourAge) > 50:
+    age = "old"
+else:
+    age = "young"
+
+print("Hello " + yourName + ". Wow you are " + age + ". Here are 5 random numbers")
+
 for i in range(5):
     print(random.random())
+
+print()
+print("Thanks for running me!!")
+print()
