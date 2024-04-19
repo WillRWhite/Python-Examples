c29f2bfc8d66b1f2d9f139680439dd2c875c4e2f
William White <will@wwhite.co.uk>
2024-04-19 22:15:53 +0100

    Added plot to logisticmap

--- sources/logisticmap.py
+++ sources/logisticmap.py
@@ -1,9 +1,20 @@
+import matplotlib.pyplot as plt
+
+
 def lmap(r,x):
     return r*x*(1-x)
 
 r = 0.4
 x = 0.5
+N=[]
+X=[]
 
-for n in range(9):
-    print(x)
+for n in range(20):
+    N.append(n)
+    X.append(x)
     x = lmap(r,x)
+print(N)
+print(X)
+
+plt.plot(X, N)
+plt.show()
