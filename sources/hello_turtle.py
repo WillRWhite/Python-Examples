f8d9652500bd0ab1f1cc59e5091bce75f325095c
William White <will@wwhite.co.uk>
2024-09-12 00:09:57 +0100

    Added a second pen

--- sources/hello_turtle.py
+++ sources/hello_turtle.py
@@ -41,13 +41,22 @@ def create_pen(colour:str,speed:int) -> Turtle:
     pen.hideturtle()
     return pen
 
-def ngon(p:Turtle, sides:int, size:int) -> None:
+def ngon(pen:Turtle, sides:int, size:int) -> None:
     for _ in range(sides):
         pen.forward(size*100/sides)
         pen.right(360/sides)
 
 pen = create_pen(colour="red",speed=0)
 ngon(pen,sides=10,size=10)
+
+pen2 = Turtle(shape="turtle", visible=True)
+pen2.speed(1)
+pen2.penup()
+pen2.goto(0,150)
+pen2.pendown()
+ngon(pen2,sides=8,size=10)
+pen2.hideturtle()
+#pen2.forward(100)
 
 done()
 
