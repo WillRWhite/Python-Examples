3c4f5ffb8e685c0e7cd84bb2f89779f68c0bee96
William White <will@wwhite.co.uk>
2024-09-16 15:31:13 +0100

    Added method configure_universe

--- sources/star_wars_lib.py
+++ sources/star_wars_lib.py
@@ -1,3 +1,5 @@
+import random
+
 class Universe():
     def __init__(self,rows:int,cols:int) -> None:
         self.rows = rows
@@ -10,19 +12,38 @@ class Universe():
                 print(self.sector[x][y], ' ', end='')
             print('')
     
+    def configure_universe(self,k_prob:float=2.0,w_prob:float=2.0, ss_prob:float=2.0) -> None:
+        #k_prob = (k_prob * self.rows * self.cols)/100
+        #prob = random.randint(1,100)
+        #k_prob = prob
+        #self.k_prob = k_prob
+        #print(self.k_prob)
+        for x in range(self.rows):
+            for y in range(self.cols):
+                prob = (random.randint(1,1000)/100)
+                if (prob <= k_prob):
+                    self.sector[x][y] = 'K'
+                elif (prob <= (k_prob + w_prob)):
+                    self.sector[x][y] = 'W'
+                elif (prob <= (k_prob + w_prob + ss_prob)):
+                    self.sector[x][y] = 'S'
+
+                
     def __repr__(self) -> str:
-        return (f"Universe(rows:int,cols:int) -> None")
+         return (f" Universe(rows:int,cols:int) -> None")
     
     def __str__(self) -> str:
-        return (f"Universe size is {self.rows} X {self.cols}")
+         return (f"Universe size is {self.rows} X {self.cols}")
 
 def main():
 
-    u1 = Universe(5,6)
-    print(repr(u1))
-    print(u1.__repr__())
-    print(u1)
+    u1 = Universe(10,10)
+    u1.configure_universe(1,0,0)
+    #print(repr(u1))
+    #print(u1.__repr__())
+    #print(u1)
     u1.print_universe()
+
  
 
 if __name__ == "__main__":
