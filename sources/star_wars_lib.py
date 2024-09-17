76e82dbef2cb643d31f209cf8d4a621277619693
William White <will@wwhite.co.uk>
2024-09-17 23:05:16 +0100

    modified:   sources/star_wars_lib.py

--- sources/star_wars_lib.py
+++ sources/star_wars_lib.py
@@ -1,22 +1,30 @@
 import random
 
+############### Parent Class Definitions ###############
+
+####################### Univewrse ######################
 class Universe:
     def __init__(self,rows:int,cols:int) -> None:
         self.rows = rows
         self.cols = cols
+        # Initilise universe sector grid. Fill each sector with a '-'
+        # indicating an empty sector
         self.sector = [['-'for x in range(cols)] for y in range(rows)]
 
-    def print_universe(self) -> None:
+    # Print the universe as a retuangular grid. Only practiclr for relatively small universes
+    def print_universe_grid(self) -> None:
         for x in range(self.rows):
             for y in range(self.cols):
                 print(self.sector[x][y], ' ', end='')
             print('')
 
-    def configure_universe(self,k_prob:float=3.0,s_prob:float=0.8, b_prob:float=0.4,w_prob:float=0.2) -> None:
+    # Populate the universe with Klingons, space stations, blackholes and Wormholes
+    def populate_universe(self,k_prob:float=3.0,s_prob:float=0.8, b_prob:float=0.4,w_prob:float=0.2) -> None:
+        # "_prob" arguments are an approximate percentage weighting for each object - Klingon, 
+        # SpaceStations Blackholes and Wormholews for each sector in the universe
         for x in range(self.rows):
             for y in range(self.cols):
                 prob = (random.randint(1,1000))/10
-                # print(prob)
                 if prob <= k_prob:
                     self.sector[x][y] = Klingon()
                 elif prob <= k_prob+s_prob:
@@ -26,6 +34,13 @@ class Universe:
                 elif prob <= k_prob+s_prob+b_prob+w_prob:
                     self.sector[x][y] = Wormhole()
 
+        # Now add the Enterprise in a firly central location. 
+        # Just obliterate whatever is there
+        x = random.randint(8, self.rows-8)
+        y = random.randint(8, self.cols-8)
+        self.sector[x][y] = Enterprise()
+
+
     def __repr__(self) -> str:
          return (f" Universe(rows:int,cols:int) -> None")
     
@@ -34,14 +49,18 @@ class Universe:
          info2 = (f"{SpaceStation.num} space stations, {Blackhole.num} black holes and {Wormhole.num} worm holes")
          return info1+info2
 
+####################### StarShip #######################
 class StarShip:
     def __init__(self):
         self.energy = 1000
     pass
-
+#################### CelestialBodie ####################
 class CelestialBodie:
     pass
 
+################ Child Class Definitions ###############
+
+######################## Klingon #######################
 class Klingon(StarShip):
     num=0
     def __init__(self):
@@ -49,7 +68,8 @@ class Klingon(StarShip):
 
     def __str__(self) -> str:
         return 'K'
-    
+
+##################### SpaceStation #####################   
 class SpaceStation(StarShip):
     num=0
     def __init__(self):
@@ -57,7 +77,14 @@ class SpaceStation(StarShip):
 
     def __str__(self) -> str:
         return 'S'
-
+    
+###################### Enterprise ######################
+class Enterprise(StarShip):
+    
+    def __str__(self) -> str:
+        return 'E'
+    
+###################### Blackhole #######################  
 class Blackhole(CelestialBodie):
     num=0
     def __init__(self):
@@ -65,7 +92,8 @@ class Blackhole(CelestialBodie):
 
     def __str__(self) -> str:
         return 'B'
-
+    
+###################### Wormkhole #######################  
 class Wormhole(CelestialBodie):
     num=0
     def __init__(self):
@@ -74,16 +102,17 @@ class Wormhole(CelestialBodie):
     def __str__(self) -> str:
         return 'W'
 
-
+######################## Main ##########################  
 def main():
 
-    u1 = Universe(10,10)
-    u1.configure_universe()
-    #print(repr(u1))
-    #print(u1.__repr__())
-    #print(u1)
-    print('')
-    u1.print_universe()
+    u1 = Universe(20,20)
+    u1.populate_universe()
+    e = Enterprise()
+    # print(repr(u1))
+    # print(u1.__repr__())
+    # print(u1)
+    # print('')
+    u1.print_universe_grid()
     print('')
     print("***************************************** STAR WARS! **********************************************")
     print('')
