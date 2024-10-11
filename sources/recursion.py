655c5b68820bc9f46cd47970eb7baf9492326e85
William White <will@wwhite.co.uk>
2024-10-11 11:03:35 +0100

    modified:   sources/recursion.py

--- sources/recursion.py
+++ sources/recursion.py
@@ -9,4 +9,19 @@ def draw(n):
         print("#", end="")
     print()
 
+def count(max_i, i=0):
+    i+=1
+    j=i
+    if i==max_i:
+        print("")
+        return i
+    else:
+        print(f"{i}",end="")
+        count(max_i,i)
+        print(f"{j}",end="")
+              
+print("")
 draw(5)
+print("")
+count(10)
+print("")
