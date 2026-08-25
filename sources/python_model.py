ff4bcf44464b5fb9f8dcb9f78476e59e54e90866
William White <will@wwhite.co.uk>
2026-08-25 12:05:50 +0100

    Added python_model.py

---
+++ sources/python_model.py
@@ -0,0 +1,38 @@
+# https://youtu.be/uti3tMcqczQ?si=lRdNaleSPs-KHHhl
+
+x = 10
+print(f"value of x is: {x}")
+print(f"type of x is: {type(x)}")
+print(f"ID of x is: {id(x)}")
+print()
+
+y = x
+
+print(f"value of y is: {y}")
+print(f"type of y is: {type(y)}")
+print(f"ID of y is: {id(y)}")
+print()
+
+x = 20
+
+print(f"value of x is: {x}")
+print(f"type of x is: {type(x)}")
+print(f"ID of x is: {id(x)}")
+print()
+
+print(f"value of y is: {y}")
+print(f"type of y is: {type(y)}")
+print(f"ID of y is: {id(y)}")
+print()
+
+print(f"value of 10 is: {10}")
+print(f"type of 10 is: {type(10)}")
+print(f"ID of 10 is: {id(10)}")
+print()
+
+print(f"value of 20 is: {20}")
+print(f"type of 20 is: {type(20)}")
+print(f"ID of 20 is: {id(20)}")
+print()
+
+
